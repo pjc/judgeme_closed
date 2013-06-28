3552b68ca9aa76a17204e7b7752ba13c0ac9cd93
Peter-Jan Celis <pj@judge.me>
2013-06-28 09:22:16 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Closedjudgeme::Application.config.secret_token = 'b7381bde560b63bd0120ef9802c80389ae7752b85a743d7d53d6e28a8120f0af15d7fcca526a1f01fc6f684b35d9430de60b9c67ac95cdb5c7e1a143aaddfee6'
